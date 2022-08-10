7e908e03873a791c065ec40cb7e393bf54fbacff
combodga <listyle7@gmail.com>
2022-08-10 20:30:34 +0300

    Removed dot import

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	. "github.com/combodga/Project/internal/app"
+	"github.com/combodga/Project/internal/app"
 )
 
 func main() {
 	const host string = ""
 	const port string = "8080"
 
-	ShortenerStart(host, port)
+	app.ShortenerStart(host, port)
 }
